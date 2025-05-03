fc1368fc689e5d73c00ae358e5a257787678284f
Boris Ghidaglia <borisghidaglia@gmail.com>
2025-05-03 21:15:04 +0200

    WIP new flow following the Figma - Adds popover for data folder and prompt gallery

--- app/(protected)/chat/page.tsx
+++ app/(protected)/chat/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 
+import { LucideBox, LucideInbox, LucideSend } from "lucide-react";
 import { useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Message } from "@/components/ui/message";
-import { LucideSend } from "lucide-react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 
 const suggestions = [
   "Can you create a gene association network for CD5, including only the 20 most co-expressed genes.",
@@ -91,7 +96,7 @@ export default function ChatPage() {
       >
         <textarea
           placeholder="Ask something to Eva"
-          className="w-full resize-none rounded-lg border border-gray-300 bg-white p-4 text-sm"
+          className="w-full resize-none scroll-p-15 rounded-lg border border-gray-300 bg-white p-4 pb-15 text-sm"
           value={input}
           onChange={(e) => setInput(e.target.value)}
           onKeyDown={handleKeyDown}
@@ -103,7 +108,79 @@ export default function ChatPage() {
         >
           <LucideSend className="size-4 -translate-x-px translate-y-px" />
         </Button>
+        <div className="relative m-3 flex items-center gap-2 self-end justify-self-start">
+          <DataFolderPopover>
+            <Button
+              size="sm"
+              className="bg-gray-200 text-xs font-semibold text-gray-500 hover:bg-gray-300"
+            >
+              <LucideInbox className="size-4" />
+              Add a dataset
+            </Button>
+          </DataFolderPopover>
+          <PromptGalleryPopover>
+            <Button
+              size="sm"
+              className="bg-gray-200 text-xs font-semibold text-gray-500 hover:bg-gray-300"
+            >
+              <LucideBox className="size-4" />
+              Eva prompt gallery
+            </Button>
+          </PromptGalleryPopover>
+        </div>
       </form>
     </div>
   );
 }
+
+const DataFolderPopover = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>{children}</PopoverTrigger>
+      <PopoverContent align="start" className="w-3xl" sideOffset={15}>
+        <div className="p-4">
+          <h3 className="text-lg font-semibold">Data Folder</h3>
+          <p className="text-sm text-gray-500">
+            Upload your own dataset to create a custom gene regulatory network.
+          </p>
+          <div className="grid grid-cols-4 gap-3 py-5">
+            <div className="bg-primary/30 h-40 rounded-lg"></div>
+            <div className="bg-primary/30 h-40 rounded-lg"></div>
+            <div className="bg-primary/30 h-40 rounded-lg"></div>
+            <div className="bg-primary/30 h-40 rounded-lg"></div>
+            <div className="bg-primary/30 h-40 rounded-lg"></div>
+            <div className="bg-primary/30 h-40 rounded-lg"></div>
+            <div className="bg-primary/30 h-40 rounded-lg"></div>
+            <div className="bg-primary/30 h-40 rounded-lg"></div>
+          </div>
+        </div>
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+const PromptGalleryPopover = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>{children}</PopoverTrigger>
+      <PopoverContent align="start" className="w-3xl" sideOffset={15}>
+        <div className="p-4">
+          <h3 className="text-lg font-semibold">Prompt Gallery</h3>
+          <p className="text-sm text-gray-500">
+            Browse through various prompts to enhance your experience.
+          </p>
+          <div className="grid grid-cols-4 gap-3 py-5">
+            <div className="bg-primary/20 h-40 rounded-lg"></div>
+            <div className="bg-primary/20 h-40 rounded-lg"></div>
+            <div className="bg-primary/20 h-40 rounded-lg"></div>
+            <div className="bg-primary/20 h-40 rounded-lg"></div>
+            <div className="bg-primary/20 h-40 rounded-lg"></div>
+            <div className="bg-primary/20 h-40 rounded-lg"></div>
+            <div className="bg-primary/20 h-40 rounded-lg"></div>
+            <div className="bg-primary/20 h-40 rounded-lg"></div>
+          </div>
+        </div>
+      </PopoverContent>
+    </Popover>
+  );
+};
